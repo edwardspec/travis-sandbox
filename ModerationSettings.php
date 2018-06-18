316e98415be9d951b8bbbdc68d0f59b61fc16035
Edward Chernenko <edwardspec@gmail.com>
2018-06-18 21:20:42 +0300

    Use memcached

--- ModerationSettings.php
+++ ModerationSettings.php
@@ -7,3 +7,6 @@ wfLoadExtension( "Moderation" );
 # Disable display_errors from DevelopmentSettings.php
 # (interferes with ModerationTestsuiteCliEngine)
 ini_set( 'display_errors', 0 );
+
+$wgMainCacheType = CACHE_MEMCACHED;
+$wgMemCachedServers = [ "127.0.0.1:11211" ];
