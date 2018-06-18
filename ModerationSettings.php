bd8646b58b0d381ad87edaf3b1649a5cbc9f9a59
Edward Chernenko <edwardspec@gmail.com>
2018-06-18 20:50:50 +0300

    Disable display_errors

--- ModerationSettings.php
+++ ModerationSettings.php
@@ -4,3 +4,6 @@ $wgEnableUploads = true;
 
 wfLoadExtension( "Moderation" );
 
+# Disable display_errors from DevelopmentSettings.php
+# (interferes with ModerationTestsuiteCliEngine)
+ini_set( 'display_errors', 0 );
