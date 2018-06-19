13cf1db26a5168d0bbc555d2b3c41e55872272c4
Edward Chernenko <edwardspec@gmail.com>
2018-06-19 15:55:31 +0300

    Cache experiment #2

---
+++ build_mediawiki.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -e -x
+###############################################################################
+# Assemble the directory with MediaWiki
+# Usage: ./build_mediawiki REL1_31
+###############################################################################
+
+branch=$1
+
+mkdir -p buildcache/mediawiki
+
+if [ ! -f buildcache/mediawiki/COMPLETE ]; then
+	(
+		cd buildcache
+		git clone --depth 1 --recursive https://gerrit.wikimedia.org/r/p/mediawiki/core.git mediawiki -b "$branch"
+
+		cd mediawiki
+		patch -p1 <../../patches/gerrit440976.patch
+
+		[[ -f includes/DevelopmentSettings.php ]] || \
+			wget https://raw.githubusercontent.com/wikimedia/mediawiki/master/includes/DevelopmentSettings.php \
+				-O includes/DevelopmentSettings.php
+
+		composer install --prefer-dist --quiet --no-interaction
+		touch COMPLETE # Mark this buildcache as usable
+	)
+fi
+
+cp -r buildcache/mediawiki ./
