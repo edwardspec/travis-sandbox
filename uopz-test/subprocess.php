37210af083a341bdfc63412aaeeafdf21d22dd27
Edward Chernenko <edwardspec@gmail.com>
2018-06-19 15:36:12 +0300

    uopz test: actually call something from MediaWiki

--- uopz-test/subprocess.php
+++ uopz-test/subprocess.php
@@ -1,11 +1,23 @@
 <?php
 
-if ( function_exists( 'uopz_set_return' ) ) {
-	uopz_set_return( 'header', function( $string, $replace = true, $http_response_code = null ) {
-		echo "Header [$string] found; replace=" . (bool)$replace . ", http_code=[", (string)$http_response_code . "]\n";
-	}, true );
+/*
+	This evaluates the performance impact of uopz_set_return()
+	being enabled when executing MediaWiki code.
+*/
+
+class UopzTest extends Maintenance {
+	public function execute() {
+		if ( function_exists( 'uopz_set_return' ) ) {
+			uopz_set_return( 'header', function( $string, $replace = true, $http_response_code = null ) {
+				echo "Header [$string] found; replace=" . (bool)$replace . ", http_code=[", (string)$http_response_code . "]\n";
+			}, true );
+		}
+
+		header( 'User-Agent: something/1.0.0' );
+		header( 'User-Agent: something/1.0.1', true );
+		header( 'Content-Type: image/png', false, 404 );
+	}
 }
 
-header( 'User-Agent: something/1.0.0' );
-header( 'User-Agent: something/1.0.1', true );
-header( 'Content-Type: image/png', false, 404 );
+$maintClass = UopzTest::class;
+require_once RUN_MAINTENANCE_IF_MAIN;
