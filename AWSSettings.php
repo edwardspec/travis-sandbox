b7f6d8dfd478f19dfaa241ebc388adc83fb749ea
Edward Chernenko <edwardspec@gmail.com>
2018-06-20 22:57:59 +0300

    Test for https://github.com/edwardspec/mediawiki-aws-s3-stable-fork

---
+++ AWSSettings.php
@@ -0,0 +1,37 @@
+<?php
+
+$KEY = getenv( 'AWS_KEY' );
+$SECRET = getenv( 'AWS_SECRET' );
+$BUCKET_PREFIX = getenv( 'AWS_BUCKET_PREFIX' );
+
+###############################################################################
+
+require_once( "$IP/extensions/AWS/AWS.php" );
+
+$wgAWSCredentials['key'] = $KEY;
+$wgAWSCredentials['secret'] = $SECRET;
+
+$wgAWSRegion = 'us-east-1'; # Northern Virginia
+
+$wgFileBackends['s3']['containerPaths'] = [
+	"$wgDBname-local-public" => "${BUCKET_PREFIX}",
+	"$wgDBname-local-thumb" => "${BUCKET_PREFIX}-thumb",
+	"$wgDBname-local-deleted" => "${BUCKET_PREFIX}-deleted",
+	"$wgDBname-local-temp" => "${BUCKET_PREFIX}-temp"
+];
+
+$wgLocalFileRepo = array (
+	'class'             => 'LocalRepo',
+	'name'              => 'local',
+	'backend'           => 'AmazonS3',
+	'scriptDirUrl'      => $wgScriptPath,
+	'scriptExtension'   => $wgScriptExtension,
+	'url'               => $wgScriptPath . '/img_auth.php',
+	'hashLevels'        => 0,
+	'zones'             => [
+		'public'  => [ 'url' => "http://${BUCKET_PREFIX}.s3.amazonaws.com" ],
+		'thumb'   => [ 'url' => "http://${BUCKET_PREFIX}-thumb.s3.amazonaws.com" ],
+		'temp'    => [ 'url' => false ],
+		'deleted' => [ 'url' => false ]
+	]
+);
