4c4f425a1bc27ef556ac9545429a2c38b7b2bf2a
Edward Chernenko <edwardspec@gmail.com>
2018-07-02 19:30:52 +0300

    Create moderator's user, use hostname, improve router.php

---
+++ ServerSettings.php
@@ -0,0 +1,6 @@
+<?php
+
+$wgServer = "http://moderation.example.com:8080";
+$wgScriptPath = "/w";
+$wgArticlePath = "/wiki/$1";
+
--- router.php
+++ router.php
@@ -1,30 +1,51 @@
 <?php
+###############################################################################
+#
+# NOT FOR PRODUCTION USE. ONLY FOR AUTOMATIC TESTS.
+#
+# Router script for built-in php-cli server (development only).
+# Serves MediaWiki.
+#
+# Usage: php -S 127.0.0.1:8080 -t /var/www/html router.php
+# ... where /var/www/html is the document root.
+#
+###############################################################################
 
-# FIXME: whitelist target scripts, sanitize PATH_INFO, etc. (as CliEngine does)
+# Folder with MediaWiki (under the document root)
+$rgScriptRoot = 'mediawiki';
 
-if ( PHP_SAPI != 'cli-server' ) {
-	echo "Not a valid entry point.";
-	return true;
-}
+# Same values as in LocalSettings.php
+$wgScriptPath = "/w";
+$wgArticlePath = "/wiki/$1";
 
-$rgDefaultScript = '/index.php';
-
-###################
-
+###############################################################################
 
 $uri = $_SERVER['REQUEST_URI'];
+
+# Rewrite 1: / -> /w/index.php
 if ( $uri === '/' ) {
-	$uri = $rgDefaultScript;
+	$uri = "$wgScriptPath/index.php";
 }
 
-$uri = preg_replace( '[^/wiki/]', $rgDefaultScript . '?title=', $uri );
-$targetScript = preg_replace( '/\?.*$/', '', $uri );
+# Rewrite 2: /wiki/Article -> /w/index.php?title=Article
+$articlePathRegex = join( '(.*)', array_map( 'quotemeta', explode( '$1', $wgArticlePath ) ) );
+$uri = preg_replace( '[^' . $articlePathRegex . '$]', '/w/index.php?title=$1', $uri );
 
-$file = realpath( $_SERVER['DOCUMENT_ROOT'] .'/'. $targetScript );
-if ( substr( $file, -4, 4 ) !== '.php' ) {
+# Rewrite 3: /w/ -> /MediaWikiFolderName/
+$aliasCount = 0; # Will become 1 if the path was rewritten
+$uri = preg_replace( '[^/w/]', "/$rgScriptRoot/", $uri, -1, $aliasCount );
+
+# Strip QUERY_STRING and find the target file
+$targetScript = preg_replace( '/\?.*$/', '', $uri );
+$path = realpath( $_SERVER['DOCUMENT_ROOT'] .'/'. $targetScript );
+if ( substr( $path, -4, 4 ) !== '.php' ) {
+	if ( $aliasCount > 0 ) {
+		header( 'Location: ' . $uri, true, 302 );
+		return true;
+	}
+
 	return false; /* Static file */
 }
 
 $_SERVER['SCRIPT_NAME'] = $uri;
-
-require_once( $file );
+require_once( $path );
