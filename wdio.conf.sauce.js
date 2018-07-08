32c1d249d514cc08eb07481f5eff57eaa1ea37c8
Edward Chernenko <edwardspec@gmail.com>
2018-07-08 20:22:34 +0300

    Check whether $wgSessionCacheType = CACHE_MEMCACHED; would mitigate the DB locking issue

--- wdio.conf.sauce.js
+++ wdio.conf.sauce.js
@@ -5,7 +5,7 @@ var merge = require( 'deepmerge' ),
 
 // Overwrite default settings
 conf = merge( conf, {
-	maxInstances: 1,
+	maxInstances: 5,
 
 	services: [ 'sauce' ],
 	user: process.env.SAUCE_USERNAME || '',
@@ -48,6 +48,6 @@ conf.capabilities = [
 	return capability;
 } );
 
-//conf.specs = [ 'specs/notify.js' ];
+conf.specs = [ 'specs/notify.js' ];
 
 exports.config = conf;
